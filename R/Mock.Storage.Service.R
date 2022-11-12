c73855b23609648626a1ede795ffd07213c66584
Flippie Coetser <flippie.coetser@gmail.com>
2022-11-12 06:54:49 -0800

    FOUNDATIONS: Implement Insert, Select, Update,  Delete

--- R/Mock.Storage.Service.R
+++ R/Mock.Storage.Service.R
@@ -3,7 +3,7 @@
 #' @description
 #' Typical operations perform on a Data Stores, for example a SQL Database, includes `Insert`, `Select`, `Update` and `Delete`.
 #' This `Storage` package provides a simple function for each of these operations while hiding all unnessecary details.
-#' 
+#'
 #' `Mock.Storage.Service()` take a configuration as input and returns a list of functions, each representing a Data Store operation.
 #' The list of functions is based on a simple Todo App example:
 #' * `Insert`, take a todo as input and adds the todo to the data store.
@@ -11,14 +11,14 @@
 #' * `SelectWhereId`, take the Id and returns the macthing todo from the data store, if it exist.
 #' * `Update`, take a todo as input and update the task and status of the todo in the data store.
 #' * `Delete`, that the Id of a todo and removes that todo from the data store if it exist.
-#' 
+#'
 #' `Mock.Storage.Service()` also includes input validation and exception handling:
 #' * The `configuration` input is expected to be a list with `DSN`, `Username` and `Password`.
 #' * The `todo` input used with `Insert` and `Update` operations is expcted to be a data frame containing an `Id`, `Task` and `Status`
-#' * The `id` input used with `SelectWhereId` and `Delete` operations is expcted to not be null. 
-#' 
+#' * The `id` input used with `SelectWhereId` and `Delete` operations is expcted to not be null.
+#'
 #' @usage NULL
-#' @returns A `list` of utility functions: 
+#' @returns A `list` of utility functions:
 #' * `[["Todo"]][["Insert"]](todo)`
 #' * `[["Todo"]][["Select"]]()`
 #' * `[["Todo"]][["SelectWhereId]](id)`
@@ -27,11 +27,11 @@
 #' @examples
 #'   configurator <- Configuration.Service()
 #'   configuration <- configurator[["GetODBCConfiguration"]]()
-#' 
+#'
 #'   operation <- configuration |> Mock.Storage.Service()
-#' 
+#'
 #'   todos <- operation[["Todo"]][["Select"]]()
-#' 
+#'
 #'   rm(configurator)
 #'   rm(configuration)
 #'   rm(operation)
@@ -41,55 +41,40 @@ Mock.Storage.Service <- \(configuration){
     storage.validate <- Mock.Storage.Validation.Service()
     configuration |> storage.validate[["Configuration"]]()
 
-    todos <- data.frame(
-        Id     = c('7ab3df6f-2e8f-44b4-87bf-3004cf1c16ae',
-               '7bfef861-6fe9-46da-9ad2-6a58779ccdcd',
-               'd3b59bf0-14f0-4444-9ec9-1913e7256ee4'),
-        Task   = c('Task.1','Task.2','Task.3'),
-        Status = c('New','New','Done')
-    )
-    
-    model.validate <- Model.Validation.Service()
+    validate <- Model.Validation.Service()
+    broker    <- configuration |> Mock.Storage.Broker()
 
     service <- list()
     service[["Todo"]] <- list()
     service[["Todo"]][["Insert"]]        <- \(todo) {
-        todo |> 
-            model.validate[["Todo"]]() |>
-            model.validate[['IsDuplicate']](todos)
-        
-        todos <<-
-            todos |>
-            dplyr::rows_insert(todo, by = "Id")
+        todos <- broker[["Todo"]][["Todos"]]
+
+        todo |>
+            validate[["Todo"]]() |>
+            validate[['IsDuplicate']](todos) |>
+            broker[['Todo']][['Insert']]()
 
         return(data.frame())
     }
     service[["Todo"]][["Select"]]        <- \(...)  {
-        todos
+        broker[['Todo']][['Select']]()
     }
     service[["Todo"]][["SelectWhereId"]] <- \(id)   {
-        id |> model.validate[["Id"]]()
-
-        todos |>
-            dplyr::filter(Id == id)
+        id |>
+            validate[["Id"]]() |>
+            broker[['Todo']][['SelectWhereId']]()
     }
     service[["Todo"]][["Update"]]        <- \(todo) {
         todo |>
-            model.validate[["Todo"]]() 
-
-        todos <<-
-            todos |>
-            dplyr::rows_update(todo, by = "Id")
+            validate[["Todo"]]() |>
+            broker[['Todo']][['Update']]()
 
         return(data.frame())
     }
     service[["Todo"]][["Delete"]]        <- \(id)   {
         id |>
-            model.validate[["Id"]]()
-
-        todos <<-
-            todos |>
-                dplyr::filter(Id != id)
+            validate[["Id"]]() |>
+            broker[['Todo']][['Delete']]()
 
         return(data.frame())
     }
--- R/ODBC.Storage.Service.R
+++ R/ODBC.Storage.Service.R
@@ -3,9 +3,9 @@
 #' @description
 #' Typical operations perform on a Data Stores, for example a SQL Database, includes `Insert`, `Select`, `Update` and `Delete`.
 #' This `Storage` package provides a simple function for each of these operations while hiding all unnessecary details.
-#' 
+#'
 #' `ODBC.Storage.Service()` take a configuration as input and returns a list of functions, each representing a Data Store operation.
-#' The first two functions: `CreateConnection` and `ExecuteQuery` is mainly used internally. 
+#' The first two functions: `CreateConnection` and `ExecuteQuery` is mainly used internally.
 #' However, users who wish to execute SQL Statement against an locally defined ODBC Data Source may find these helpfull.
 #' The second set of functions is based on a simple Todo App example:
 #' * `Insert`, take a todo as input and adds the todo to the data store.
@@ -13,14 +13,14 @@
 #' * `SelectWhereId`, take the Id and returns the macthing todo from the data store, if it exist.
 #' * `Update`, take a todo as input and update the task and status of the todo in the data store.
 #' * `Delete`, that the Id of a todo and removes that todo from the data store if it exist.
-#' 
+#'
 #' `ODBC.Storage.Service()` also includes input validation and exception handling:
 #' * The `configuration` input is expected to be a list with `DSN`, `Username` and `Password`.
 #' * The `todo` input used with `Insert` and `Update` operations is expcted to be a data frame containing an `Id`, `Task` and `Status`
-#' * The `id` input used with `SelectWhereId` and `Delete` operations is expcted to not be null. 
-#' 
+#' * The `id` input used with `SelectWhereId` and `Delete` operations is expcted to not be null.
+#'
 #' @usage NULL
-#' @returns A `list` of utility functions: 
+#' @returns A `list` of utility functions:
 #' * `CreateConnection()`
 #' * `ExecuteQuery(sql)`
 #' * `[["Todo"]][["Insert"]](todo)`
@@ -31,11 +31,11 @@
 #' @examples
 #'   configurator <- Configuration.Service()
 #'   configuration <- configurator[["GetODBCConfiguration"]]()
-#' 
+#'
 #'   operation <- configuration |> ODBC.Storage.Service()
-#' 
+#'
 #'   todos <- operation[["Todo"]][["Select"]]()
-#' 
+#'
 #'   rm(configurator)
 #'   rm(configuration)
 #'   rm(operation)
@@ -45,75 +45,38 @@ ODBC.Storage.Service <- \(configuration) {
   storage.validate <- ODBC.Storage.Validation.Service()
   configuration |>  storage.validate[["Configuration"]]()
 
-  storage.exception  <- ODBC.Storage.Exceptions()
-  model.validate     <- Model.Validation.Service()
+  validate     <- Model.Validation.Service()
+  broker <- configuration |> ODBC.Storage.Broker()
 
   service <- list()
-  service[["CreateConnection"]] <- \() {
-    tryCatch(
-      DBI::dbConnect(
-        odbc::odbc(),
-        dsn = configuration[["DSN"]],
-        uid = configuration[["Username"]],
-        pwd = configuration[["Password"]]
-      ),
-      error=storage.exception[["ConnectionExceptions"]]
-    )
-  }
-  service[["ExecuteQuery"]] <- \(query) {
-    connection <- service[["CreateConnection"]]()
-
-    tryCatch(
-      response <-
-        connection |>
-          DBI::dbGetQuery(query),
-      error=storage.exception[["QueryExceptions"]]
-    )
-
-    connection |>
-      DBI::dbDisconnect()
-
-    return(response)
-  }
-
   service[["Todo"]] <- list()
   service[["Todo"]][["Insert"]]        <- \(todo) {
-    todo |> model.validate[["Todo"]]()
-    stringr::str_glue(
-      "INSERT INTO [dbo].[Todo] ",
-      "(Id, Task, Status) ",
-      "VALUES ('{todo[['Id']]}', '{todo[['Task']]}', '{todo[['Status']]}')") |>
-        service[["ExecuteQuery"]]()
+    todo |>
+        validate[["Todo"]]() |>
+        broker[["Todo"]][["Insert"]]()
+
+    return(data.frame())
   }
   service[["Todo"]][["Select"]]        <- \()     {
-    stringr::str_glue(
-      "SELECT LOWER([Id]) as Id ,[Task],[Status] ",
-      "FROM [dbo].[Todo]") |>
-      service[["ExecuteQuery"]]()
+    broker[["Todo"]][["Select"]]()
   }
   service[["Todo"]][["SelectWhereId"]] <- \(id)   {
-    id |> model.validate[["Id"]]()
-    stringr::str_glue(
-      "SELECT LOWER([Id]) as Id ,[Task],[Status] ",
-      "FROM [dbo].[Todo]",
-      "WHERE Id = '{id}'") |>
-      service[["ExecuteQuery"]]()
+    id |>
+        validate[["Id"]]() |>
+        broker[["Todo"]][["SelectWhereId"]]()
   }
   service[["Todo"]][["Update"]]        <- \(todo) {
-    todo |> model.validate[["Todo"]]()
-    stringr::str_glue(
-      "UPDATE [dbo].[ToDo] ",
-      "SET Task = '{todo[['Task']]}', Status = '{todo[['Status']]}' ",
-      "WHERE Id = '{todo[['Id']]}'") |>
-      service[["ExecuteQuery"]]()
+    todo |>
+        validate[["Todo"]]() |>
+        broker[["Todo"]][["Update"]]()
+
   }
   service[["Todo"]][["Delete"]]        <- \(id)   {
-    id |> model.validate[["Id"]]()
-    stringr::str_glue(
-      "DELETE ",
-      "FROM [dbo].[ToDo] ",
-      "WHERE Id = '{id}'") |>
-      service[["ExecuteQuery"]]()
+    id |>
+        validate[["Id"]]() |>
+        broker[["Todo"]][["Delete"]]()
+
+    return(data.frame())
   }
   return(service)
 }
