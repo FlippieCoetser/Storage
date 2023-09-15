b3b5f9e06d123ab213a281e4c51c944397ae3aad
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-15 11:04:35 -0700

    BROKER: operation[['GetManualConfig']]() ReturnsManualConfig -> PASS

--- R/Storage.Configuration.Broker.R
+++ R/Storage.Configuration.Broker.R
@@ -1,25 +1,31 @@
 Storage.Configuration.Broker <- \() {
   utility <- Environment::Environment()
+  exception <- Storage.Configuration.Exceptions()
 
   operations <- list()
-  operations[["OpenConfigFile"]]  <- \() {
-    utility[["OpenConfigFile"]]()
+  operations[['OpenConfigFile']]  <- \() {
+    utility[['OpenConfigFile']]()
   }
-  operations[["GetPresetConfig"]] <- \() {
-    configuration <- list()
-    configuration[['dsn']] <- "DSN" |> utility[["GetEnvVariable"]]()
-    configuration[['uid']] <- "UID" |> utility[["GetEnvVariable"]]()
-    configuration[['pwd']] <- "PWD" |> utility[["GetEnvVariable"]]()
-    return(configuration)
+  operations[['GetPresetConfig']] <- \() {
+    tryCatch({
+      configuration <- list()
+      configuration[['dsn']] <- 'DSN' |> utility[['GetEnvVariable']]()
+      configuration[['uid']] <- 'UID' |> utility[['GetEnvVariable']]()
+      configuration[['pwd']] <- 'PWD' |> utility[['GetEnvVariable']]()
+      return(configuration)
+    }, 
+    error = exception[['Configuration']])
   }
   operations[['GetManualConfig']] <- \() {
+    tryCatch({
     configuration <- list()
-    configuration[['driver']]   <- "DRIVER" |> utility[['GetEnvVariable']]()
-    configuration[['server']]   <- "SERVER" |> utility[['GetEnvVariable']]()
-    configuration[['database']] <- "DATABASE" |> utility[['GetEnvVariable']]()
-    configuration[['uid']]      <- "UID" |> utility[['GetEnvVariable']]()
-    configuration[['pwd']]      <- "PWD" |> utility[['GetEnvVariable']]()
-    return(configuration)
+    configuration[['driver']]   <- 'DRIVER' |> utility[['GetEnvVariable']]()
+    configuration[['server']]   <- 'SERVER' |> utility[['GetEnvVariable']]()
+    configuration[['database']] <- 'DATABASE' |> utility[['GetEnvVariable']]()
+    configuration[['uid']]      <- 'UID' |> utility[['GetEnvVariable']]()
+    configuration[['pwd']]      <- 'PWD' |> utility[['GetEnvVariable']]()
+    return(configuration)},
+    error = exception[['Configuration']])
   }
   return(operations)
 }
--- R/Storage.Configuration.Exceptions.R
+++ R/Storage.Configuration.Exceptions.R
@@ -1,18 +1,57 @@
 Storage.Configuration.Exceptions <- \() {
   exception <- list()
-  exception[['dsnIsNull']] <- \(invoke) {
+  exception[['dsnIsNull']]     <- \(invoke) {
     if (invoke) {
       stop('Configuration has no dsn', call. = FALSE)
     }
   }
-  exception[['uidIsNull']] <- \(invoke) {
+  exception[['uidIsNull']]     <- \(invoke) {
     if (invoke) {
       stop('Configuration has no uid', call. = FALSE)
     }
   }
-  exception[['pwdIsNull']] <- \(invoke) {
+  exception[['pwdIsNull']]     <- \(invoke) {
     if (invoke) {
       stop('Configuration has no pwd', call. = FALSE)
+    }
+  }
+  exception[['Configuration']] <- \(error) {
+    'DSN'      |> grepl(error) |> exception[['NoDSN']]()
+    'DRIVER'   |> grepl(error) |> exception[['NoDRIVER']]()
+    'SERVER'   |> grepl(error) |> exception[['NoSERVER']]()
+    'DATABASE' |> grepl(error) |> exception[['NoDATABASE']]()
+    'UID'      |> grepl(error) |> exception[['NoUID']]()
+    'PWD'      |> grepl(error) |> exception[['NoPWD']]()
+    stop(error, call. = FALSE)
+  }
+  exception[['NoDSN']]         <- \(invoke) {
+    if (invoke) {
+      stop('No DSN environment variable defined. check .Renviron file.', call. = FALSE)
+    }
+  }
+  exception[['NoDRIVER']]      <- \(invoke) {
+    if (invoke) {
+      stop('No DRIVER environment variable defined. check .Renviron file.', call. = FALSE)
+    }
+  }
+  exception[['NoSERVER']]      <- \(invoke) {
+    if (invoke) {
+      stop('No SERVER environment variable defined. check .Renviron file.', call. = FALSE)
+    }
+  }
+  exception[['NoDATABASE']]    <- \(invoke) {
+    if (invoke) {
+      stop('No DATABASE environment variable defined. check .Renviron file.', call. = FALSE)
+    }
+  }
+  exception[['NoUID']]         <- \(invoke) {
+    if (invoke) {
+      stop('No UID environment variable defined. check .Renviron file.', call. = FALSE)
+    }
+  }
+  exception[['NoPWD']]         <- \(invoke) {
+    if (invoke) {
+      stop('No PWD environment variable defined. check .Renviron file.', call. = FALSE)
     }
   }
   return(exception)
