b4480924190838b139b343bd979875538b4201c5
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-16 05:59:16 -0700

    BROKER: operation[['GetPresentConfig']]() ReturnsValidConfigIfExist -> FAIL

--- tests/testthat/test-ODBC.Configuration.Broker.R
+++ tests/testthat/test-ODBC.Configuration.Broker.R
@@ -20,3 +20,56 @@ describe("When operations <- ODBC.Configuration.Broker()",{
     operations[['GetPresetConfig']] |> expect.exist()
   })
 })
+
+describe("When configuration <- operation[['GetPresetConfig']]()",{
+  it("then configuration is a list",{
+    # Given
+    operations <- ODBC.Configuration.Broker()
+
+    # When
+    configuration <- operations[['GetPresetConfig']]()
+
+    # Then
+    configuration |> expect.list()
+  })
+  it("then configuration contains drv parameter",{
+    # Given
+    operations <- ODBC.Configuration.Broker()
+
+    # When
+    configuration <- operations[['GetPresetConfig']]()
+
+    # Then
+    configuration[['drv']] |> expect.exist()
+  })
+  it("then configuration contains dsn parameter",{
+    # Given
+    operations <- ODBC.Configuration.Broker()
+
+    # When
+    configuration <- operations[['GetPresetConfig']]()
+
+    # Then
+    configuration[['dsn']] |> expect.exist()
+  })
+  it("then configuration contains uid parameter",{
+    # Given
+    operations <- ODBC.Configuration.Broker()
+
+    # When
+    configuration <- operations[['GetPresetConfig']]()
+
+    # Then
+    configuration[['uid']] |> expect.exist()
+  })
+  it("then configuration contains pwd parameter",{
+    # Given
+    operations <- ODBC.Configuration.Broker()
+
+    # When
+    configuration <- operations[['GetPresetConfig']]()
+
+    # Then
+    configuration[['pwd']] |> expect.exist()
+  })
+})
