e3a069dd4f0b802ca44bdc5ad84fc3ba6bd1967b
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-16 04:56:33 -0700

    EXCEPTIONS: input |> exception[['dsnNotFound']]() ThrowsExceptionIfInputTRUE -> PASS

--- R/ODBC.Configuration.Exceptions.R
+++ R/ODBC.Configuration.Exceptions.R
@@ -1,5 +1,9 @@
 ODBC.Configuration.Exceptions <- \() {
   exceptions <- list()
-  exceptions[['dsnNotFound']] <- \() {}
+  exceptions[['dsnNotFound']] <- \(invoke) {
+    if(invoke) {
+      stop("No DSN environment variable not found in .Renviron Configuration file.")
+    }
+  }
   return(exceptions)
 }
