6ab58bd17d9938a6959df2e13381a63a5b1ee00d
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-16 05:04:30 -0700

    EXCEPTIONS: Update Exception Call Signature

--- R/ODBC.Configuration.Exceptions.R
+++ R/ODBC.Configuration.Exceptions.R
@@ -2,12 +2,12 @@ ODBC.Configuration.Exceptions <- \() {
   exceptions <- list()
   exceptions[['dsnNotFound']] <- \(invoke) {
     if(invoke) {
-      stop("No DSN environment variable not found in .Renviron Configuration file.")
+      stop("No DSN environment variable not found in .Renviron Configuration file.", call. = FALSE)
     }
   }
   exceptions[['uidNotFound']] <- \(invoke) {
     if(invoke) {
-      stop("No UID environment variable not found in .Renviron Configuration file.")
+      stop("No UID environment variable not found in .Renviron Configuration file.", call. = FALSE)
     }
   }
   return(exceptions)
