6d17613d48f7c2bf6266816c43c1ab83360ca816
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-16 05:58:45 -0700

    BROKER: operation[['GetPresentConfig']]() ReturnsValidConfigIfExist -> PASS

--- R/ODBC.Configuration.Broker.R
+++ R/ODBC.Configuration.Broker.R
@@ -1,5 +1,17 @@
-ODBC.Configuration.Broker <- \() {
+ODBC.Configuration.Broker <- \(environment = Environment::Environment()) {
+  exception <- ODBC.Configuration.Exceptions()
+
   operations <- list()
-  operations[['GetPresetConfig']] <- \() {}
+  operations[['GetPresetConfig']] <- \() {
+    tryCatch({
+      list(
+        drv = odbc::odbc(),
+        dsn = 'DSN' |> environment[['GetEnvVariable']](),
+        uid = 'UID' |> environment[['GetEnvVariable']](),
+        pwd = 'PWD' |> environment[['GetEnvVariable']]()
+      )
+    }, 
+    error = exception[['Configuration']])
+  }
   return(operations)
 }
