f4e018c178ab30cd4a7936756337fa64b5b2f253
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-16 06:54:30 -0700

    EXCEPTIONS: ODBC.Configuration.Exceptions() ReturnsListContainingDriverNotFoundException -> PASS

--- R/ODBC.Configuration.Exceptions.R
+++ R/ODBC.Configuration.Exceptions.R
@@ -10,6 +10,7 @@ ODBC.Configuration.Exceptions <- \() {
       stop("No DSN environment variable not found in .Renviron Configuration file.", call. = FALSE)
     }
   }
+  exceptions[['driverNotFound']] <- \() {}
   exceptions[['uidNotFound']] <- \(invoke) {
     if(invoke) {
       stop("No UID environment variable not found in .Renviron Configuration file.", call. = FALSE)
