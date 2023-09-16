bd41fbf4a319e6f7894302d46e4fdb2fcefd376e
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-16 04:20:32 -0700

    BROKER: ODBC.Configuration.Broker() ReturnsListContainingGetPresetConfigOperation -> FAIL

--- tests/testthat/test-ODBC.Configuration.Broker.R
+++ tests/testthat/test-ODBC.Configuration.Broker.R
@@ -12,4 +12,11 @@ describe("When operations <- ODBC.Configuration.Broker()",{
     # Then
     operations |> expect.list()
   })
+  it("then operations contains GetPresetConfig operation",{
+    # When
+    operations <- ODBC.Configuration.Broker()
+
+    # Then
+    operations[['GetPresetConfig']] |> expect.exist()
+  })
 })
