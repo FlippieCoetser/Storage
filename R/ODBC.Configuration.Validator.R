591068ffd60cbd1f42d8294216d58909bed6bdec
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 07:11:51 -0700

    VALIDATOR: configuration |> validate[['HasDrv']]() ThrowsAnExceptionIfConfigurationHasNoDrv -> PASS

--- R/ODBC.Configuration.Validator.R
+++ R/ODBC.Configuration.Validator.R
@@ -1,8 +1,12 @@
 ODBC.Configuration.Validator <- \() {
+  exception <- ODBC.Configuration.Exceptions()
+
   validations <- list()
   validations[['PresetConfig']] <- \() {}
   validations[['ManualConfig']] <- \() {}
-  validations[['HasDrv']]       <- \() {}
+  validations[['HasDrv']]       <- \(configuration) {
+    configuration[['drv']] |> is.null() |> exception[['drvIsNull']]()
+  }
   validations[['HasDsn']]       <- \() {}
   validations[['HasDriver']]    <- \() {}
   validations[['HasServer']]    <- \() {}
