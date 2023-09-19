a5825c9968385fc2e7acf075966409e48d730726
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 03:24:05 -0700

    VALIDATOR: ODBC.Configuration.Validator() ReturnsListContainingHasDatabaseValidator -> PASS

--- R/ODBC.Configuration.Validator.R
+++ R/ODBC.Configuration.Validator.R
@@ -2,5 +2,6 @@ ODBC.Configuration.Validator <- \() {
   validations <- list()
   validations[['HasDsn']] <- \() {}
   validations[['HasDriver']] <- \() {}
+  validations[['HasDatabase']] <- \() {}
   return(validations)
 }
