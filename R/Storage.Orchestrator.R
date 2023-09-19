203fc0fedb78fb6ae4e1f7a57e4c8b1917c5560d
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 11:49:44 -0700

    ORCHESTRATOR: query |> orchestrate[['ExecuteQuery']]() ThrowsExceptionIfOrchestratorTypeMock -> PASS

--- R/Storage.Orchestrator.R
+++ R/Storage.Orchestrator.R
@@ -1,5 +1,12 @@
-Storage.Orchestrator <- \() {
+Storage.Orchestrator <- \(configuration, type, data = NULL) {
+  services <- list()
+  services[['Mock']] <- \() configuration |> Mock.Storage.Broker(data)
+
+  service <- services[[type]]()
+
   orchestrations <- list()
-  orchestrations[['ExecuteQuery']] <- \() {}
+  orchestrations[['ExecuteQuery']] <- \(query) {
+    query |> service[['ExecuteQuery']]()
+  }
   return(orchestrations)
 }
