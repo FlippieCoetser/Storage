75dbc2c06828a1dbd86ac231a87abd576755f886
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 04:38:57 -0700

    VALIDATOR: ODBC.Configuration.Validator() ReturnsListContainingPresentConfigValidator -> PASS

--- R/ODBC.Configuration.Validator.R
+++ R/ODBC.Configuration.Validator.R
@@ -1,11 +1,12 @@
 ODBC.Configuration.Validator <- \() {
   validations <- list()
-  validations[['HasDrv']]      <- \() {}
-  validations[['HasDsn']]      <- \() {}
-  validations[['HasDriver']]   <- \() {}
-  validations[['HasServer']]   <- \() {}
-  validations[['HasDatabase']] <- \() {}
-  validations[['HasUid']]      <- \() {}
-  validations[['HasPwd']]      <- \() {}
+  validations[['PresetConfig']] <- \() {}
+  validations[['HasDrv']]       <- \() {}
+  validations[['HasDsn']]       <- \() {}
+  validations[['HasDriver']]    <- \() {}
+  validations[['HasServer']]    <- \() {}
+  validations[['HasDatabase']]  <- \() {}
+  validations[['HasUid']]       <- \() {}
+  validations[['HasPwd']]       <- \() {}
   return(validations)
 }
