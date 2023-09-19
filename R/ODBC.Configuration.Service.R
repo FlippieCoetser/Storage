ab3ace8755a9cc239ef15dc24ce7311c4566c423
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 09:04:49 -0700

    SERVICE: ODBC.Configuration.Service Configuration Validation

--- R/ODBC.Configuration.Service.R
+++ R/ODBC.Configuration.Service.R
@@ -1,13 +1,17 @@
 ODBC.Configuration.Service <- \(broker) {
+  validate <- ODBC.Configuration.Validator()
+
   services <- list()
   services[['OpenConfigFile']]  <- \() {
     broker[['OpenConfigFile']]()
   }
   services[['GetPresetConfig']] <- \() {
-    broker[['GetPresetConfig']]()
+    broker[['GetPresetConfig']]() |>
+    validate[['PresetConfig']]()
   }
   services[['GetManualConfig']] <- \() {
-    broker[['GetManualConfig']]()
+    broker[['GetManualConfig']]() |>
+    validate[['ManualConfig']]()
   }
   return(services)
 }
