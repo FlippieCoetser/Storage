293512876a47b76604f7e6b2d3dc8f88af4a8ebd
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 12:09:45 -0700

    ORCHESTRATOR: Orchestration Passthrough

--- R/Storage.Orchestrator.R
+++ R/Storage.Orchestrator.R
@@ -9,10 +9,20 @@ Storage.Orchestrator <- \(configuration, type = 'Mock', data = NULL) {
   orchestrations[['ExecuteQuery']] <- \(query) {
     query |> service[['ExecuteQuery']]()
   }
-  orchestrations[['Insert']]       <- \() {}
-  orchestrations[['Select']]       <- \() {}
-  orchestrations[['SelectWhereId']]<- \() {}
-  orchestrations[['Update']]       <- \() {}
-  orchestrations[['Delete']]       <- \() {}
+  orchestrations[['Insert']]       <- \(entity, table) {
+    entity |> service[['Insert']](table)
+  }
+  orchestrations[['Select']]       <- \(fields, table) {
+    fields |> service[['Select']](table)
+  }
+  orchestrations[['SelectWhereId']]<- \(fields, table, id) {
+    fields |> service[['SelectWhereId']](table, id)
+  }
+  orchestrations[['Update']]       <- \(entity, table) {
+    entity |> service[['Update']](table)
+  }
+  orchestrations[['Delete']]       <- \(id, table) {
+    id |> service[['Delete']](table)
+  }
   return(orchestrations)
 }
