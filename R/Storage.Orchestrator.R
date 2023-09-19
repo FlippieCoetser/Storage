60b47aa0c098bb47cb986e82af8db4a3dd88a6f6
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-19 11:52:20 -0700

    ORCHESTRATOR: query |> orchestrate[['ExecuteQuery']]() ThrowsNoExceptionIfOrchestratorTypeODBC -> PASS

--- R/Storage.Orchestrator.R
+++ R/Storage.Orchestrator.R
@@ -1,6 +1,7 @@
 Storage.Orchestrator <- \(configuration, type, data = NULL) {
   services <- list()
   services[['Mock']] <- \() configuration |> Mock.Storage.Broker(data)
+  services[['ODBC']] <- \() configuration |> ODBC.Storage.Broker()
 
   service <- services[[type]]()
 
