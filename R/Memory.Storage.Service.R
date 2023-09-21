f16a87926f21f02137e3f8ec9ae2251008e7db4c
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-21 08:42:12 -0700

    SERVICE: Memory.Storage.Service() ReturnsListContainingSelect -> PASS

--- R/Memory.Storage.Service.R
+++ R/Memory.Storage.Service.R
@@ -2,5 +2,6 @@ Memory.Storage.Service <- \() {
   services <- list()
   services[['ExecuteQuery']] <- \() {}
   services[['Insert']]       <- \() {}
+  services[['Select']]       <- \() {}
   return(services)
 }
