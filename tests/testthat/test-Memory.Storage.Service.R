709aef0ad2ff47228f3b636701064ffb5c55a8c5
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-21 08:36:16 -0700

    SERVICE: Memory.Storage.Service() ReturnsListOfServices -> FAIL

--- tests/testthat/test-Memory.Storage.Service.R
+++ tests/testthat/test-Memory.Storage.Service.R
@@ -3,3 +3,13 @@ describe('Memory.Storage.Service',{
     Memory.Storage.Service |> expect.exist()
   })
 })
+
+describe("When services <- Memory.Storage.Service()",{
+  it("then services is a list",{
+    # When
+    services <- Memory.Storage.Service()
+
+    # Then
+    services |> expect.list()
+  })
+})
