31b53554eadd7f4e1af302e15db1f6d144dbdf7b
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-21 11:36:10 -0700

    DOCUMENTATION: Update Documentation

--- R/ODBC.Storage.Service.R
+++ R/ODBC.Storage.Service.R
@@ -1,17 +1,3 @@
-#' Storage Server
-#'
-#' @description
-#' Provide an storage interface with basic CRUD Operations with ability to switch between MOCK or ODBC Storage Provider.
-#' 
-#' @usage NULL
-#' @returns A `list` of functions: 
-#' * `ExecuteQuery(query)`
-#' * `Insert(entity, table)`
-#' * `Select(fields, table)`
-#' * `SelectWhereId(fields, table, id)`
-#' * `Update(entity, table)`
-#' * `Delete(id, table)`
-#' @export
 ODBC.Storage.Service <- \(broker) {
   validate <- ODBC.Storage.Validator()
   
