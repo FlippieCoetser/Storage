36afd87f0ff2236b04cabb883b195559d6417118
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-21 10:38:08 -0700

    EXCEPTIONS: input |> exception[['NoExecuteQuery']]() ThrowsExceptionIsInputTRUE -> FAIL

--- tests/testthat/test-Memory.Storage.Exceptions.R
+++ tests/testthat/test-Memory.Storage.Exceptions.R
@@ -20,3 +20,28 @@ describe("When exceptions <- Memory.Storage.Exception()",{
    exceptions[['NoExecuteQuery']] |> expect.exist()
   })
 })
+
+describe("When input |> exception[['NoExecuteQuery']]()", {
+  it("then no exception is thrown if input is FALSE", {
+    # Given
+    exception <- Memory.Storage.Exceptions()
+    
+    # When
+    input <- FALSE
+    
+    # Then
+    input |> exception[['NoExecuteQuery']]() |> expect.no.error()
+  })
+  it("then an exception is thrown if input is TRUE", {
+    # Given
+    exception <- Memory.Storage.Exceptions()
+    
+    expected.error <- "Memory Storage Provider has no ExecuteQuery implementation."
+    
+    # When
+    input <- TRUE
+    
+    # Then
+    input |> exception[['NoExecuteQuery']]() |> expect.error(expected.error)
+  })
+})
