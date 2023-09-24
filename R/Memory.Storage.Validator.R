591eb58dedd28d0134323d87c418855ff48d014b
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-24 07:36:57 -0700

    VALIDATION: entity |> validate[['IsNew']](table) ThrowsExceptionIfEntityIsNotNew -> PASS

--- R/Memory.Storage.Validator.R
+++ R/Memory.Storage.Validator.R
@@ -1,10 +1,14 @@
-Memory.Storage.Validator <- \() {
+Memory.Storage.Validator <- \(broker = NULL) {
   exception <- Memory.Storage.Exceptions()
 
   validators <- list()
   validators[['NoImplementation']] <- \() {
     TRUE |> exception[['NoExecuteQuery']]()
   }
-  validators[['IsNew']] <- \() {}
+  validators[['IsNew']] <- \(entity, table) {
+    match.count <- entity[['Id']] |> broker[['SelectWhereId']](table) |> nrow() 
+    (match.count != 0) |> exception[['DuplicateId']]()
+    return(entity)
+  }
   return(validators)
 }
