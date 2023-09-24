7bc3f65769b400a1de94a65b02be34d85099e308
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-24 08:26:50 -0700

    CODE RUB: Align Indentation

--- R/Memory.Storage.Exceptions.R
+++ R/Memory.Storage.Exceptions.R
@@ -5,12 +5,12 @@ Memory.Storage.Exceptions <- \() {
       stop('Memory Storage Provider Error: ExecuteQuery not implemented.', call. = FALSE)
     }
   }
-  exceptions[['DuplicateId']] <- \(invoke) {
+  exceptions[['DuplicateId']]    <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: Duplicate Id not allowed.', call. = FALSE)
     }
   }
-  exceptions[['InvalidTable']] <- \(invoke, table) {
+  exceptions[['InvalidTable']]   <- \(invoke, table) {
     if(invoke) {
       stop('Memory Storage Provider Error: ', table, 'is not a valid table.', call. = FALSE)
     }
