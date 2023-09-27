7655cdb4223d77e965c35da68a642e1aa843ffe4
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-27 10:13:13 -0700

    CODE RUB: Align Indentation

--- R/Memory.Storage.Broker.R
+++ R/Memory.Storage.Broker.R
@@ -2,36 +2,36 @@ Memory.Storage.Broker <- \(configuration = NULL) {
   tables <- list()
 
   operations <- list()
-  operations[['CreateTable']]      <- \(model, table) {
+  operations[['CreateTable']]   <- \(model, table) {
     tables[[table]] <<- model
     return(NULL)
    }
-  operations[['Seed']]             <- \(data, table) {
+  operations[['Seed']]          <- \(data, table) {
     tables[[table]] <<- tables[[table]] |> rbind(data)
     return(NULL)
   }
-  operations[['GetTableNames']]    <- \() {
+  operations[['GetTableNames']] <- \() {
     tables |> names()
   }
-  operations[['ExecuteQuery']]     <- \(query) {
+  operations[['ExecuteQuery']]  <- \(query) {
     return(data.frame())
   }
-  operations[['Insert']]           <- \(entity, table) {
+  operations[['Insert']]        <- \(entity, table) {
     tables[[table]] <<- tables[[table]] |> rbind(entity)
     return(data.frame())
   }
-  operations[['Select']]           <- \(table, fields = NULL) {
+  operations[['Select']]        <- \(table, fields = NULL) {
     tables[[table]]
   }
-  operations[['SelectWhereId']]    <- \(id, table, fields = NULL) {
+  operations[['SelectWhereId']] <- \(id, table, fields = NULL) {
     tables[[table]][tables[[table]][['Id']] == id,]
   }
-  operations[['Update']]           <- \(entity, table) {
+  operations[['Update']]        <- \(entity, table) {
     condition <- tables[[table]][['Id']] == entity[['Id']]
     tables[[table]][condition,] <<- entity
     return(data.frame())      
   }
-  operations[['Delete']]           <- \(id, table) {
+  operations[['Delete']]        <- \(id, table) {
     condition <- tables[[table]][['Id']] != id
     tables[[table]] <<- tables[[table]][condition,]
     return(data.frame())
