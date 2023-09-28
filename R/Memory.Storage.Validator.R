7abd1bcf4a4d76e1dcb5752d6e3d010c4c689665
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-28 01:28:01 -0700

    VALIDATION: Memory.Storage.Validator() ReturnsListContainingModelValidator -> PASS

--- R/Memory.Storage.Validator.R
+++ R/Memory.Storage.Validator.R
@@ -2,6 +2,7 @@ Memory.Storage.Validator <- \(broker = NULL) {
   exception <- Memory.Storage.Exceptions()
 
   validators <- list()
+  validators[['Model']]            <- \() {}
   validators[['NoImplementation']] <- \() {
     TRUE |> exception[['NoExecuteQuery']]()
   }
