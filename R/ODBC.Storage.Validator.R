1e0583692b65e9ed451f90f8157e25cc2141bea1
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-29 11:50:01 -0700

    VALIDATION: ODBC.Storage.Validator() ReturnsListContainingNotNULLValidator -> PASS

--- R/ODBC.Storage.Validator.R
+++ R/ODBC.Storage.Validator.R
@@ -1,9 +1,10 @@
 ODBC.Storage.Validator <- \() {
   exception <- ODBC.Storage.Exceptions()
   validators <- list()
-  validators[['Entity']] <- \() {}
-  validators[['Table']]  <- \() {}
-  validators[['Id']]     <- \() {}
+  validators[['Entity']]  <- \() {}
+  validators[['Table']]   <- \() {}
+  validators[['Id']]      <- \() {}
+  validators[['NotNULL']] <- \() {}
   validators[['Query']]  <- \(query) {
     query |> is.null() |> exception[['QueryIsNull']]()
     return(query)
