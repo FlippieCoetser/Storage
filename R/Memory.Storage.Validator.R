7c9fc17b120964f083b86dec504ed622fadf2593
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-29 06:54:57 -0700

    VALIDATION: Memory.Storage.Validator() ReturnsListContainingNotNULLValidator -> PASS

--- R/Memory.Storage.Validator.R
+++ R/Memory.Storage.Validator.R
@@ -2,6 +2,7 @@ Memory.Storage.Validator <- \(broker = NULL) {
   exception <- Memory.Storage.Exceptions()
 
   validators <- list()
+  validators[['NotNULL']]          <- \() {}
   validators[['IsDataFrame']]      <- \(input) {
     input |> is.data.frame() |> isFALSE() |> exception[['InvalidType']]('data.frame')
     return(input)
