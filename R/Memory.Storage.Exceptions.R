eaac485460be2cab5c2645912151c27df118b4f5
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-29 09:42:44 -0700

    EXCEPTIONS: Memory.Storage.Exception() ReturnsListContainingInvalidIdentifier -> PASS

--- R/Memory.Storage.Exceptions.R
+++ R/Memory.Storage.Exceptions.R
@@ -1,36 +1,37 @@
 Memory.Storage.Exceptions <- \() {
   exceptions <- list()
-  exceptions[['NoExecuteQuery']] <- \(invoke) {
+  exceptions[['NoExecuteQuery']]    <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: ExecuteQuery not implemented.', call. = FALSE)
     }
   }
-  exceptions[['DuplicateId']]    <- \(invoke) {
+  exceptions[['DuplicateId']]       <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: Duplicate Id not allowed.', call. = FALSE)
     }
   }
-  exceptions[['EntityNotFound']] <- \(invoke) {
+  exceptions[['EntityNotFound']]    <- \(invoke) {
     if(invoke) {
       stop('Memory Storage Provider Error: Entity not found.', call. = FALSE)
     }
   }
-  exceptions[['InvalidTable']]   <- \(invoke, table) {
+  exceptions[['InvalidTable']]      <- \(invoke, table) {
     if(invoke) {
       stop('Memory Storage Provider Error: ', table, ' is not a valid table.', call. = FALSE)
     }
   }
-  exceptions[['InvalidType']]    <- \(invoke, type) {
+  exceptions[['InvalidType']]       <- \(invoke, type) {
     if(invoke) {
       stop("Memory Storage Provider Error: Invalid Type. Expected '", type,"'.", call. = FALSE)
     }
   }
-  exceptions[['InvalidRows']]    <- \(invoke, count) {
+  exceptions[['InvalidRows']]       <- \(invoke, count) {
     if(invoke) {
       stop('Memory Storage Provider Error: Invalid number of rows. Expected ', count, ' rows.', call. = FALSE)
     }
   }
-  exceptions[['IsNULL']]         <- \(invoke, variable) {
+  exceptions[['InvalidIdentifier']] <- \() { }
+  exceptions[['IsNULL']]            <- \(invoke, variable) {
     if(invoke) {
       stop('Memory Storage Provider Error: ',variable,' is NULL.', call. = FALSE)
     }
