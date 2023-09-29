f6ff395093bf08b99753093418f45b8e800e6284
Flippie Coetser <flippie.coetser@gmail.com>
2023-09-29 11:51:16 -0700

    VALIDATION: ODBC.Storage.Validator() ReturnsListContainingIsCharactersValidator -> PASS

--- R/ODBC.Storage.Validator.R
+++ R/ODBC.Storage.Validator.R
@@ -1,10 +1,11 @@
 ODBC.Storage.Validator <- \() {
   exception <- ODBC.Storage.Exceptions()
   validators <- list()
-  validators[['Entity']]  <- \() {}
-  validators[['Table']]   <- \() {}
-  validators[['Id']]      <- \() {}
-  validators[['NotNULL']] <- \() {}
+  validators[['Entity']]       <- \() {}
+  validators[['Table']]        <- \() {}
+  validators[['Id']]           <- \() {}
+  validators[['NotNULL']]      <- \() {}
+  validators[['IsCharacters']] <- \() {}
   validators[['Query']]  <- \(query) {
     query |> is.null() |> exception[['QueryIsNull']]()
     return(query)
