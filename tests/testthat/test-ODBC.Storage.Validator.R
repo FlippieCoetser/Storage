2194d125e1151bc282f7bf2bffec1662953bf455
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-01 01:34:33 -0700

    VALIDATION: input |> validate[['NotNULL']](name) ReturnsInputIfInputNotNULL  -> FAIL

--- tests/testthat/test-ODBC.Storage.Validator.R
+++ tests/testthat/test-ODBC.Storage.Validator.R
@@ -144,4 +144,18 @@ describe("When input |> validate[['NotNULL']](name)",{
     # Then
     input |> validate[['NotNULL']](name) |> expect.error(expected.error)
   })
+  it("then input is returned if input is not NULL",{
+    # Given
+    validate <- ODBC.Storage.Validator()
+    
+    input <- list()
+    
+    expected <- input
+    
+    # When
+    actual <- input |> validate[['NotNULL']]()
+    
+    # Then
+    actual |> expect.equal(expected)
+  })
 })
