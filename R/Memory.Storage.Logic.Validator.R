0e8b0362a14f9c9395d3cf6bd850a3781a463b93
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-02 09:41:33 -0700

    VALIDATION: Memory.Storage.Logic.Validator() ReturnsListContainingIs.New.EntityValidator -> PASS

--- R/Memory.Storage.Logic.Validator.R
+++ R/Memory.Storage.Logic.Validator.R
@@ -7,5 +7,6 @@ Memory.Storage.Logic.Validator <- \(broker = NULL) {
       isFALSE() |> exception[['Table.Invalid']](table)
     return(table)
   }
+  validators[['Is.New.Entity']] <- \() {}
   return(validators)
 }
