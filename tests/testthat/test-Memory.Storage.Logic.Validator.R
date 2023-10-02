6ff9aa621806426ffffdbb7513486f0977269bfe
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-02 09:32:41 -0700

    VALIDATION: table |> validate[['Is.Existing.Table']]() ThrowsExceptionIfTableDoesNotExist -> FAIL

--- tests/testthat/test-Memory.Storage.Logic.Validator.R
+++ tests/testthat/test-Memory.Storage.Logic.Validator.R
@@ -20,3 +20,61 @@ describe('When validators <- Memory.Storage.Logic.Validator()',{
     validators[['Is.Existing.Table']] |> expect.exist()
   })
 })
+
+describe("When table |> validate[['Is.Existing.Table']]()",{
+  it("then no exception is thrown if table exist",{
+    # Given
+    table <- 'Todo'
+    
+    configuration <- data.frame()
+
+    broker <- configuration |> Memory.Storage.Broker()
+
+
+    Todo.Mock.Data |> broker[['Seed.Table']](table)
+
+    validator <- broker |> Memory.Storage.Logic.Validator()
+
+    valid.table <- table
+    
+    # Then
+    valid.table |> validator[['Is.Existing.Table']]() |> expect.no.error()
+  })
+  it("then an exception is thrown if table does not exist",{
+    # Given
+    configuration <- data.frame()
+
+    broker <- configuration |> Memory.Storage.Broker()
+    Todo.Mock.Data |> broker[['Seed.Table']](table)
+
+    validator <- broker |> Memory.Storage.Logic.Validator()
+
+    invalid.table <- 'Table.Invalid'
+    
+    expected.error <- 'Memory Storage Provider Error: Table.Invalid is not a valid table.'
+    
+    # Then
+    invalid.table |> validator[['Is.Existing.Table']]() |> expect.error(expected.error)
+  })
+  it("then table is returned if table exist",{
+    # Given
+    table <- 'Todo'
+    
+    configuration <- data.frame()
+
+    broker <- configuration |> Memory.Storage.Broker()
+
+    Todo.Mock.Data |> broker[['Seed.Table']](table)
+
+    validator <- broker |> Memory.Storage.Logic.Validator()
+
+    valid.table    <- table
+    expected.table <- valid.table
+    
+    # When
+    actual.table <- valid.table |> validator[['Is.Existing.Table']]()
+
+    # Then
+    actual.table |> expect.equal(expected.table)
+  })
+})
