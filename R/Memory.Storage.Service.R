4c48886a34180c026b387aee76d4527c178999b7
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-02 10:22:39 -0700

    CODE RUB: Add Function Composition

--- R/Memory.Storage.Service.R
+++ R/Memory.Storage.Service.R
@@ -4,60 +4,66 @@ Memory.Storage.Service <- \(broker) {
   
   services <- list()
   services[['Create.Table']]    <- \(model, table) {
-    model |> validate.structure[['Model']]()
     table |> validate.structure[['Table']]()
 
-    model |> broker[['Create.Table']](table)
+    model |> 
+      validate.structure[['Model']]() |> 
+      broker[['Create.Table']](table)
   }
   services[['Seed.Table']]      <- \(data, table) {
-    data  |> validate.structure[['Data']]()
     table |> validate.structure[['Table']]()
 
-    data  |> broker[['Seed.Table']](table)
+    data |> 
+      validate.structure[['Data']]() |> 
+      broker[['Seed.Table']](table)
   }
   services[['Execute.Query']]   <- \(...) {
     TRUE |> validate.structure[['NoImplementation']]()
     ...  |> broker[['Execute.Query']]()
   }
   services[['Add']]             <- \(entity, table) {
-    entity |> validate.structure[['Entity']]()
-    table  |> validate.structure[['Table']]()
+    table |> 
+      validate.structure[['Table']]() |> 
+      validate.logic[['Is.Existing.Table']]()
 
-    table  |> validate.logic[['Is.Existing.Table']]()
-    entity |> validate.logic[['Is.New.Entity']](table)
-
-    entity |> broker[['Insert']](table)
+    entity |> 
+      validate.structure[['Entity']]() |> 
+      validate.logic[['Is.New.Entity']](table) |> 
+      broker[['Insert']](table)
   }
   services[['Retrieve']]        <- \(table, fields) {
-    table |> validate.structure[['Table']]()
-    table |> validate.logic[['Is.Existing.Table']]()
-
-    table |> broker[['Select']](fields)
+    table |> 
+      validate.structure[['Table']]() |> 
+      validate.logic[['Is.Existing.Table']]() |> 
+      broker[['Select']](fields)
   }
   services[['RetrieveWhereId']] <- \(id, table, fields) {
-    id    |> validate.structure[['Id']]()
-    table |> validate.structure[['Table']]()
+    table |> 
+      validate.structure[['Table']]() |> 
+      validate.logic[['Is.Existing.Table']]()
 
-    table |> validate.logic[['Is.Existing.Table']]()
-
-    id |> broker[['SelectWhereId']](table, fields)
+    id |> 
+      validate.structure[['Id']]() |> 
+      broker[['SelectWhereId']](table, fields)
   }
   services[['Modify']]          <- \(entity, table) {
-    entity |> validate.structure[['Entity']]()
-    table  |> validate.structure[['Table']]()
+    table  |> 
+      validate.structure[['Table']]()  |> 
+      validate.logic[['Is.Existing.Table']]()
 
-    table  |> validate.logic[['Is.Existing.Table']]()
-    entity |> validate.logic[['Is.Existing.Entity']](table)
-
-    entity |> broker[['Update']](table)
+    entity |> 
+      validate.structure[['Entity']]() |> 
+      validate.logic[['Is.Existing.Entity']](table) |> 
+      broker[['Update']](table)
   }
   services[['Remove']]          <- \(id, table) {
-    id    |> validate.structure[['Id']]()
-    table |> validate.structure[['Table']]()
-    
-    table |> validate.logic[['Is.Existing.Table']]()
-    
-    id |> broker[['Delete']](table)
+    table |> 
+      validate.structure[['Table']]() |>
+      validate.logic[['Is.Existing.Table']]()
+
+    id |> 
+      validate.structure[['Id']]() |> 
+      broker[['Delete']](table)
   }
   return(services)
 }
