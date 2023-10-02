af89f92e9db430ebfb181b6667e4397f8b5bcae1
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-02 09:32:49 -0700

    VALIDATION: table |> validate[['Is.Existing.Table']]() ThrowsExceptionIfTableDoesNotExist -> PASS

--- R/Memory.Storage.Logic.Validator.R
+++ R/Memory.Storage.Logic.Validator.R
@@ -1,5 +1,11 @@
-Memory.Storage.Logic.Validator <- \() {
+Memory.Storage.Logic.Validator <- \(broker = NULL) {
+  exception <- Memory.Storage.Exceptions()
+
   validators <- list()
-  validators[['Is.Existing.Table']] <- \() {}
+  validators[['Is.Existing.Table']] <- \(table) {
+    broker[['Get.Tables']]() |> is.element(table) |> 
+      isFALSE() |> exception[['Table.Invalid']](table)
+    return(table)
+  }
   return(validators)
 }
