ee2a60ef430a0176de10fe8912c9d5671d4ba53a
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-03 08:45:01 -0700

    BROKER: table |> operate[['Select']]() ReturnsDataFrameIfTableExist

--- tests/testthat/test-ODBC.Storage.Broker.R
+++ tests/testthat/test-ODBC.Storage.Broker.R
@@ -68,3 +68,17 @@ describe("when query |> operate[['Execute.Query']]()",{
     output |> expect.data.frame()
   })
 })
+
+describe("When table |> operate[['Select']]()",{
+  it('then a data.frame is returned if table exist in storage',{
+    skip_if_not(environment == 'local')
+    # Given
+    operate <- configuration |> ODBC.Storage.Broker()
+
+    # When
+    entities <- table |> operate[['Select']]() 
+    
+    # Then
+    entities |> expect.data.frame()
+  })
+})
