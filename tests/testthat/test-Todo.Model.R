3563ddf58d00d4eca2b14e52cf0c6ba5e44a74e7
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-07 08:02:27 -0700

    MODEL: task |> Todo.Model() ReturnsDataFrame -> FAIL

--- tests/testthat/test-Todo.Model.R
+++ tests/testthat/test-Todo.Model.R
@@ -3,3 +3,16 @@ describe('Todo.Model',{
     Todo.Model |> expect.exist()
   })
 })
+
+describe("When model <- task |> Todo.Model()",{
+  it("then model is a data.frame",{
+    # Given 
+    task <- 'Task'
+
+    # When
+    model <- task |> Todo.Model()
+
+    # Then
+    model |> expect.data.frame()
+  })
+})
