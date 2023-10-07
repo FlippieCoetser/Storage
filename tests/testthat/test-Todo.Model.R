3f229a6549491b600494a317c3c7976704b72441
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-07 08:22:59 -0700

    MODEL: Todo.Model() ReturnsDataFrameContainingIdColumnOfCharacterType -> FAIL

--- tests/testthat/test-Todo.Model.R
+++ tests/testthat/test-Todo.Model.R
@@ -12,4 +12,12 @@ describe("When model <- Todo.Model()",{
     # Then
     model |> expect.data.frame()
   })
+  it("then model has Id column of type character",{
+    # When
+    model <- Todo.Model()
+
+    # Then
+    model[['Id']] |> expect.exist()
+    model[['Id']] |> expect.character()
+  })
 })
