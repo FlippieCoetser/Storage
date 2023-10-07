8aaba2c54f77abaa0e63f36a51ee62b383c0b4c3
Flippie Coetser <flippie.coetser@gmail.com>
2023-10-07 08:19:52 -0700

    MODEL: Todo.Model() ReturnsDataFrame -> FAIL

--- tests/testthat/test-Todo.Model.R
+++ tests/testthat/test-Todo.Model.R
@@ -3,3 +3,13 @@ describe('Todo.Model',{
     Todo.Model |> expect.exist()
   })
 })
+
+describe("When model <- Todo.Model()",{
+  it("then model is a data.frame",{
+    # When
+    model <- Todo.Model()
+
+    # Then
+    model |> expect.data.frame()
+  })
+})
